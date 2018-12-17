7a128f7320b2ee564cc59e093648526e527c7637
Bobbynurfebriyanto1234 <bobby.febriyanto@students.amikom.ac.id>
2018-12-17 16:58:54 +0700

    tugas4

--- src/konversiMataUang.java
+++ src/konversiMataUang.java
@@ -5,9 +5,10 @@
 	    public static void main(String[] args) {
 	        Scanner masukan = new Scanner(System.in);
 	        float rupiah, dolar;
-	        System.out.print("Masukan Dolar = ");
+	        System.out.print("Masukan Dolar Terbanyak = ");
 	        dolar = masukan.nextFloat();
 	        rupiah = (float) (dolar*14000);
+	        
 	        System.out.println();
 	        System.out.println("Hasil Kurs = ");
 	        System.out.println("Rupiah = " + rupiah);
