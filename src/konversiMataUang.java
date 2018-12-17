40e7446c44f07b5033b38002f64f291101555027
Bobbynurfebriyanto1234 <bobby.febriyanto@students.amikom.ac.id>
2018-12-17 16:25:53 +0700

    Tugas1

---
+++ src/konversiMataUang.java
@@ -0,0 +1,17 @@
+	import java.util.Scanner;
+
+	public class konversiMataUang {
+
+	    public static void main(String[] args) {
+	        Scanner masukan = new Scanner(System.in);
+	        float rupiah, dolar;
+	        System.out.print("Masukan Dolar = ");
+	        dolar = masukan.nextFloat();
+	        rupiah = (float) (dolar*14000);
+	        System.out.println();
+	        System.out.println("Hasil Kurs = ");
+	        System.out.println("Rupiah = " + rupiah);
+	        System.out.println("Dolar = " + dolar);
+	    }
+	}
+
